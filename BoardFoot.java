9fcdc69dc61a268cf5fb77e2635e71169a14feee
Alex Kapajika <alex.kapajika.ntumba@stu.ocsb.ca>
2025-03-18 15:22:04 +0000

    Assignment

--- BoardFoot.java
+++ BoardFoot.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /**
  * @author Alex Kapajika
  * @version 1.0
@@ -22,29 +23,33 @@ public final class BoardFoot {
      * The required length in inches.
      * @return this is the return arg.
      */
-    public static double calculateBoardFoot(final double width, final double height) {
+    public static float calculateBoardFoot(final float width,
+     final float height) {
         return LENGTH / (width * height);
     }
+    /**
+     * @param args
+     */
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double width, height;
-        while (true) {
-            try {
-                System.out.print("Enter the width of the wood: ");
-                width = Double.parseDouble(scanner.nextLine());
-                System.out.print("Enter the height of the wood: ");
-                height = Double.parseDouble(scanner.nextLine());
-                if (width > 0 && height > 0) {
-                    double length = calculateBoardFoot(width, height);
-                    System.out.printf("The length is: %.2f inches\n", length);
-                    break;
-                } else {
-                    System.out.println("Invalid input");
-                }
-            } catch (NumberFormatException e) {
-                System.out.println("Invalid input.");
-                System.out.println("the value cannot be a negative number.");
+        try {
+            // Prompt the user to enter the width and height of the wood.
+            System.out.print("Enter the width of the wood: ");
+            float width = Float.parseFloat(scanner.nextLine());
+            System.out.print("Enter the height of the wood: ");
+            float height = Float.parseFloat(scanner.nextLine());
+            // Calculate the length of the wood.
+            if (width <= 0 && height <= 0) {
+                // the value cannot be a negative number.
+                System.out.println("Invalid input");
+            } else {
+                float length = calculateBoardFoot(width, height);
+                // Display the length of the wood.
+                System.out.printf("The length is: %.2f inches\n", length);
             }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input.");
+            System.out.println("the value cannot be a negative number.");
         }
     }
 }
